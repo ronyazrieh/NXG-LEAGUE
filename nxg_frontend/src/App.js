c32cf836498bfa29e40742c11eb01ea8ee309aef
Rony Azrieh <ronyazrieh@cmail.carleton.ca>
2024-02-14 09:25:46 -0500

    Enhanced home-page, and added a start nav_bar

--- nxg_frontend/src/App.js
+++ nxg_frontend/src/App.js
@@ -1,11 +1,26 @@
 import logo from './logo.svg';
 import './App.css';
-import { Route, Link } from "react-router-dom";
-import Home from './Home';
+import { Link, Route, Routes } from "react-router-dom";
+import  Home from './Home';
+import  ContactUs  from './ContanctUs';
 
 function App() {
   return (
-      <Home />
+    <>
+      <div className='nav-bar'>
+        <nav>
+          <ul>
+            <li><Link to="/">Home</Link></li>
+            <li><Link to="/ContactUs">Contact Us</Link></li>
+          </ul>
+        </nav>
+      </div>
+
+    <Routes>
+      <Route path="/" element={<Home />} />
+      <Route path="/ContactUs" element={<ContactUs />} />
+    </Routes>
+    </>
   );
 }
 
