c167e0d4755985496bbf94a71922b72941df96bd
Rony Azrieh <ronyazrieh@cmail.carleton.ca>
2024-02-14 20:48:33 -0500

    Near completion for navigation bar.

---
+++ nxg_frontend/src/NavBar.js
@@ -0,0 +1,32 @@
+import React from "react";
+import { Link } from "react-router-dom";
+import SUI from "./SUI.png";
+import './NavBar.css';
+
+function NavBar(){
+    return(
+        <div className='nav-bar'>
+            <nav>
+                <div id="nav-container">
+                    <ul className="nav-row">
+                        <li><Link textDecoration='inherit'to="/" className="links">Home</Link></li>
+                        <li><Link to="/RegisterNow" className="links">Register Now</Link></li>
+                        <li><Link to="/ContactUs" className="links">Contact Us</Link></li>
+                    </ul>
+
+                    <img src={SUI} id="logo"/>
+
+                    <ul className="nav-row">
+                        <li><Link to="/Divisions" className="links">Divisions</Link></li>
+                        <li><Link to="/Events" className="links">Events</Link></li>
+                        <li><Link to="/" className="links">Coming Soon</Link></li>
+                    </ul>
+
+                </div>
+            </nav>
+        </div>
+    )
+}
+
+
+export default NavBar
